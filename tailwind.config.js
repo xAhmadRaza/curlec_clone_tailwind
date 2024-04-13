91eefad5266c57782095a3ded2dc4474a4da2e7b
xAhmadRaza <Gintoki.sakata789@gmail.com>
2024-04-14 04:52:48 +0500

    feat: added responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,6 +35,14 @@ module.exports = {
         },
       },
     },
+    screens: {
+      xs: "320px",
+      sm: "480px",
+      md: "768px",
+      lg: "992px",
+      xl: "1120px",
+      "2xl": "1300px",
+    },
   },
   plugins: [],
 };
